feb25ae75ace0a09feed09c097a05ec3fbc0313d
Gary Yue <garyyue2017@gmail.com>
2019-10-26 18:16:02 -0700

    added heroku fix

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const app = express();
 
 let PORT = process.env.PORT;
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  //Set static folder
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
-})
+  console.log(`Listening on PORT ${PORT}`);
+});
